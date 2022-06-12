785e8e5bd1e547e1893d97751edbbef51c782d12
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2022-06-12 14:35:04 +0530

    Implemented logic for Adding product from product details page to checkout_page.dart

--- lib/data/controllers/checkout_controller.dart
+++ lib/data/controllers/checkout_controller.dart
@@ -1,11 +1,13 @@
 import 'package:cycle_store/data/models/address_model.dart';
+import 'package:cycle_store/data/models/product_model.dart';
 import 'package:cycle_store/data/services/user_service.dart';
 import 'package:cycle_store/utils/utils.dart';
 import 'package:get/get.dart';
 
 class CheckoutController extends GetxController {
-  List products = Get.arguments["products"];
-  List quantities = Get.arguments["quantities"];
+  List? products = Get.arguments["products"];
+  List? quantities = Get.arguments["quantities"];
+  Product? product = Get.arguments["product"];
 
   RxList<Address> defaultAddress = <Address>[].obs;
   RxDouble price = 0.0.obs;
@@ -15,9 +17,15 @@ class CheckoutController extends GetxController {
 
   @override
   void onInit() {
-    for (int i = 0; i < products.length; i++) {
-      price.value =
-          price.value + (products[i]["product"].price * quantities[i]);
+    // Products from cart
+    if (products != null) {
+      for (int i = 0; i < products!.length; i++) {
+        price.value =
+            price.value + (products![i]["product"].price * quantities![i]);
+      }
+      // Product from product details page
+    } else {
+      price.value = product!.price;
     }
 
     getDefaultAddress();
